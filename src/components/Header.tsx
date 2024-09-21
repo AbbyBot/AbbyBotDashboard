de2bb02b6c70d606df2c29728290de983c8be491
astronautmarkus <astronautmarkus@gmail.com>
2024-09-21 16:29:03 -0300

    change lucide-react for fontawesome

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import abbyLogo from '../assets/abbybot-logo.png';
-import { ChartLine, Globe, LogIn, ChevronDown, Globe2, Github, ExternalLink } from 'lucide-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine, faGlobe, faRightToBracket, faChevronDown, faExternalLinkAlt, faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { DISCORD_BASE_URL } from '../env';
 import { useState } from 'react';
-
 export function Header() {
     const [dropdownOpen, setDropdownOpen] = useState(false);
 
@@ -10,62 +11,64 @@ export function Header() {
         setDropdownOpen(!dropdownOpen);
     };
 
-    return <header className="bg-navbar header">
-        <nav className='container navbar'>
-            <div style={{ width: "100%", display: "flex" }}>
-                <section className="brand">
-                    <img draggable="false" src={abbyLogo} alt="" width={50} height={50} />
-                    <button className='btn-link text-condensed'>
-                        AbbyBot-Dashboard
-                    </button>
-                </section>
-                <section>
+    return (
+        <header className="bg-navbar header">
+            <nav className='container navbar'>
+                <div style={{ width: "100%", display: "flex" }}>
+                    <section className="brand">
+                        <img draggable="false" src={abbyLogo} alt="" width={50} height={50} />
+                        <button className='btn-link text-condensed'>
+                            AbbyBot-Dashboard
+                        </button>
+                    </section>
+                    <section>
+                        <ul className='nav'>
+                            <li className='nav-item'>
+                                <button className='btn-link text-condensed'>
+                                    <FontAwesomeIcon icon={faChartLine} color="white" size="sm" />
+                                    Dashboard
+                                </button>
+                            </li>
+                            <li className='nav-item text-condensed dropdown'>
+                                <button className='btn-link text-condensed' onClick={toggleDropdown}>
+                                    <FontAwesomeIcon icon={faEarthAmericas} color="white" size="sm" />
+                                    AbbyBotProject
+                                    <FontAwesomeIcon icon={faChevronDown} color="white" size="sm" />
+                                </button>
+                                {dropdownOpen && (
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <a href="https://abbybot.cl">
+                                                <FontAwesomeIcon icon={faGlobe} size="sm" /> AbbyBot (website)
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://github.com/abbybot">
+                                                <FontAwesomeIcon icon={faGithub} size="sm" /> GitHub Organization
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="https://reyesandfriends.cl/">
+                                                <FontAwesomeIcon icon={faExternalLinkAlt} size="sm" /> reyesandfriends.cl
+                                            </a>
+                                        </li>
+                                    </ul>
+                                )}
+                            </li>
+                        </ul>
+                    </section>
+                </div>
+                <div>
                     <ul className='nav'>
                         <li className='nav-item'>
-                            <button className='btn-link text-condensed'>
-                                <ChartLine color='white' size={16} />
-                                Dashboard
+                            <button className='btn-link text-condensed' onClick={() => window.location.href = `${DISCORD_BASE_URL}`}>
+                                <FontAwesomeIcon icon={faRightToBracket} color="white" size="sm" />
+                                Login
                             </button>
-                        </li>
-                        <li className='nav-item text-condensed dropdown'>
-                            <button className='btn-link text-condensed' onClick={toggleDropdown}>
-                                <Globe color='white' size={16} />
-                                AbbyBotProject
-                                <ChevronDown color='white' size={16} />
-                            </button>
-                            {dropdownOpen && (
-                                    <ul className="dropdown-menu">
-                                    <li>
-                                        <a href="https://abbybot.cl">
-                                            <Globe2 size={16} /> AbbyBot (website)
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://github.com/abbybot">
-                                            <Github size={16} /> GitHub Organization
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://reyesandfriends.cl/">
-                                            <ExternalLink size={16} /> reyesandfriends.cl
-                                        </a>
-                                    </li>
-                                </ul>
-                            )}
                         </li>
                     </ul>
-                </section>
-            </div>
-            <div>
-                <ul className='nav'>
-                    <li className='nav-item'>
-                        <button className='btn-link text-condensed' onClick={() => window.location.href = `${DISCORD_BASE_URL}`}>
-                            <LogIn color='white' size={16} />
-                            Login
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+                </div>
+            </nav>
+        </header>
+    );
 }
